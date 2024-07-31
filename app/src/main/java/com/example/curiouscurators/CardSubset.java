26168dd881ee29a59bdd1c9bf30b842db39da4b9
Logan <logan@gatlintc.com>
2024-07-31 00:41:05 -0500

    Implemented binary search

--- app/src/main/java/com/example/curiouscurators/CardSubset.java
+++ app/src/main/java/com/example/curiouscurators/CardSubset.java
@@ -1,6 +1,7 @@
 package com.example.curiouscurators;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CardSubset {
     public enum FilterType {
@@ -14,7 +15,7 @@ public class CardSubset {
         this.filter = "";
         this.filterType = FilterType.Name;
         this.contained = new ArrayList<>();
-        this.resetAndFilter();
+        this.applyFilter(this.getSearchSet());
     }
     private ArrayList<String[]> getSearchSet() {
         switch (this.filterType) {
@@ -25,25 +26,59 @@ public class CardSubset {
             case Set:
                 return Card.getCardsBySet();
         }
-        // Unreachable
         return null;
     }
 
-    private void resetAndFilter() {
-        this.contained.clear();
-        ArrayList<String[]> searchSet = this.getSearchSet();
-        assert searchSet != null;
-        for (String[] card : searchSet) {
-            String name = card[0];
-            if (name.startsWith(this.filter)) {
-                this.contained.add(card);
+
+    private void applyFilter(ArrayList<String[]> searchSet) {
+        ArrayList<String[]> next = new ArrayList<>();
+        System.out.println(searchSet.size());
+        int wordLength = this.filter.length();
+        // Use binary search to find the range of values which begin with the filter
+        int low = 0, high = searchSet.size() - 1, rangeStart = -1, rangeEnd = -1;
+        // Find start
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            String atMiddle = searchSet.get(middle)[0];
+            String substring = atMiddle.substring(0, Integer.min(wordLength, atMiddle.length()));
+            if (substring.compareTo(this.filter) > 0) {
+                high = middle - 1;
+            } else if (substring.compareTo(this.filter) < 0) {
+                low = middle + 1;
+            } else {
+                rangeStart = middle;
+                high = middle - 1;
             }
         }
-    }
+        if (rangeStart == -1) {
+            this.contained.clear();
+            return;
+        }
+        // Find end
+        low = 0;
+        high = searchSet.size() - 1;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            String atMiddle = searchSet.get(middle)[0];
+            String substring = atMiddle.substring(0, Integer.min(wordLength, atMiddle.length()));
+            if (substring.compareTo(this.filter) > 0) {
+                high = middle - 1;
+            } else if (substring.compareTo(this.filter) < 0) {
+                low = middle + 1;
+            } else {
+                rangeEnd = middle;
+                low = middle + 1;
+            }
+        }
 
-    private void keepAndFilter() {
-        ArrayList<String[]> next = new ArrayList<>();
-        for (String[] card : this.contained) {
+        if (rangeEnd == -1) {
+            this.contained.clear();
+            return;
+        }
+
+        List<String[]> reducedList = searchSet.subList(rangeStart, rangeEnd);
+
+        for (String[] card : reducedList) {
             String name = card[0];
             if (name.startsWith(this.filter)) {
                 next.add(card);
@@ -60,15 +95,17 @@ public class CardSubset {
         newFilter = Card.cleanName(newFilter);
         if (newFilter.startsWith(this.filter)) {
             this.filter = newFilter;
-            this.keepAndFilter();
+            this.applyFilter(this.contained);
         } else {
             this.filter = newFilter;
-            this.resetAndFilter();
+            this.contained.clear();
+            this.applyFilter(this.getSearchSet());
         }
     }
 
     public void setFilterType(FilterType filterType) {
         this.filterType = filterType;
-        this.resetAndFilter();
+        this.contained.clear();
+        this.applyFilter(this.getSearchSet());
     }
 }
